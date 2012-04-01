bf4db2a45102231406866deccd3bf4c90b78df2e
ajlopez <webmaster@ajlopez.com>
2012-04-01 19:16:54 -0300

    Tuple with tests

---
+++ Src/SetTuples.Tests/TupleTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SetTuples.Tests
+{
+    [TestClass]
+    public class TupleTests
+    {
+        [TestMethod]
+        public void GetDefaultValues()
+        {
+            Tuple tuple = new Tuple(2);
+
+            Assert.IsNull(tuple[0]);
+            Assert.IsNull(tuple[1]);
+        }
+
+        [TestMethod]
+        public void SetAndGetValues()
+        {
+            Tuple tuple = new Tuple(2);
+
+            tuple[0] = "Adam";
+            tuple[1] = 800;
+
+            Assert.AreEqual("Adam", tuple[0]);
+            Assert.AreEqual(800, tuple[1]);
+        }
+    }
+}
---
+++ Src/SetTuples/Tuple.cs
@@ -0,0 +1,24 @@
+﻿namespace SetTuples
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Tuple
+    {
+        private object[] values;
+
+        public Tuple(int size)
+        {
+            this.values = new object[size];
+        }
+
+        public object this[int index] 
+        {
+            get { return this.values[index]; }
+
+            set { this.values[index] = value; }
+        }
+    }
+}
