016558ca0d3e588741d24a14c470f092d23790c2
ajlopez <webmaster@ajlopez.com>
2012-04-01 20:07:57 -0300

    Add Tuple with test

--- Src/SetTuples.Tests/SetTests.cs
+++ Src/SetTuples.Tests/SetTests.cs
@@ -27,5 +27,29 @@ namespace SetTuples.Tests
             Assert.AreEqual("Name", set.Columns.First().Name);
             Assert.AreEqual("Age", set.Columns.Skip(1).First().Name);
         }
+
+        [TestMethod]
+        public void AddTuple()
+        {
+            Column[] columns = new Column[] {
+                new Column("Name", ColumnType.String),
+                new Column("Age", ColumnType.Integer)
+            };
+
+            Set set = new Set(columns);
+
+            NameValues values = new NameValues();
+            values.Add("Name", "Adam");
+            values.Add("Age", 800);
+
+            var result = set.AddTuple(values);
+
+            Assert.IsNotNull(result.Tuples);
+            Assert.AreEqual(1, result.Tuples.Count());
+
+            Tuple tuple = result.Tuples.First();
+            Assert.AreEqual("Adam", tuple[0]);
+            Assert.AreEqual(800, tuple[1]);
+        }
     }
 }
--- Src/SetTuples/Set.cs
+++ Src/SetTuples/Set.cs
@@ -9,7 +9,7 @@
     {
         private Column[] columns;
         private Dictionary<string, int> positions;
-        private IEnumerable<Tuple> tuples;
+        private IList<Tuple> tuples;
 
         public Set(IEnumerable<Column> columns)
         {
@@ -22,8 +22,28 @@
             this.tuples = new List<Tuple>();
         }
 
+        private Set(Set set)
+        {
+            this.columns = set.columns;
+            this.positions = set.positions;
+            this.tuples = new List<Tuple>(set.tuples);
+        }
+
         public IEnumerable<Tuple> Tuples { get { return this.tuples; } }
 
         public IEnumerable<Column> Columns { get { return this.columns; } }
+
+        public Set AddTuple(NameValues values)
+        {
+            Set set = new Set(this);
+            Tuple tuple = new Tuple(this.columns.Length);
+            
+            foreach (string name in values.Values.Keys)
+                tuple[this.positions[name]] = values.Values[name];
+
+            set.tuples.Add(tuple);
+
+            return set;
+        }
     }
 }
